dc04a98c5023d0e7adf040038e2cab4f22f9f11c
rina shkrabova <eshkrab@gmail.com>
2023-06-20 23:11:00 -0700

    broadcast option?

--- app/app.py
+++ app/app.py
@@ -65,6 +65,7 @@ async def udp_server():
 
 async def udp_heartbeat():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.setblocking(False)
 
     while True:
